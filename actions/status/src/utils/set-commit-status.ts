e607fb8359a3faaa21c88c30b4f2db54cc42b915
Thomas Knickman <tknickman@gmail.com>
2025-08-06 21:40:50 -0400

    fix(actions/status): lookup job by id
    build
    
    Updates
    
    remove action

--- actions/status/src/utils/set-commit-status.ts
+++ actions/status/src/utils/set-commit-status.ts
@@ -1,8 +1,47 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import type { Context } from '@actions/github/lib/context';
 import { getStatusForJob } from './get-status-for-job';
 import { wait } from './wait';
 import type { WorkflowStage, Job } from '../types';
+
+function getRequiredContext(context: Context):
+  | {
+      sha: string;
+      environment?: string;
+      projectName?: string;
+    }
+  | undefined {
+  if (context.eventName === 'repository_dispatch') {
+    const sha = context.payload.client_payload?.git?.sha;
+    const environment = context.payload.client_payload?.environment;
+    const projectName = context.payload.client_payload?.project?.name;
+    if (!sha) {
+      core.warning('Missing required context. Skipping status update.');
+      return undefined;
+    }
+
+    return {
+      sha,
+      environment,
+      projectName,
+    };
+  }
+
+  /**
+   * workflow_dispatch is the same as repository_dispatch in that the SHA in the context is always set to HEAD,
+   * but it's triggered by a workflow run so we can't guarantee what the intended SHA is. The best way to handle this
+   * is use an input to specify the SHA and set a commit status for that SHA manually using action script.
+   */
+  if (context.eventName === 'workflow_dispatch') {
+    core.warning('workflow_dispatch is not supported');
+    return undefined;
+  }
+
+  return {
+    sha: context.sha,
+  };
+}
 
 export async function setCommitStatus({
   stage,
@@ -10,29 +49,21 @@ export async function setCommitStatus({
   stage: WorkflowStage;
 }): Promise<void> {
   const context = github.context;
-
-  if (context.eventName !== 'repository_dispatch') {
-    core.warning(
-      `This is not a repository_dispatch event: eventName=${context.eventName}`,
-    );
-    return;
-  }
-
-  const sha = context.payload.client_payload?.git?.sha;
-  const environment = context.payload.client_payload?.environment;
-  const projectName = context.payload.client_payload?.project?.name;
-  if (!sha || !environment || !projectName) {
-    core.warning(
-      'Missing required fields in client_payload. Skipping status update.',
-    );
-    return;
-  }
-
-  const requiredContext = `(${projectName} - ${environment})`;
   const token = core.getInput('github_token');
-  const defaultStatusName = `${context.workflow}/${context.job} ${requiredContext}`;
-  const contextForStatus = core.getInput('name') || defaultStatusName;
   const octokit = github.getOctokit(token);
+
+  const requiredContext = getRequiredContext(context);
+  if (requiredContext === undefined) {
+    core.warning('Missing required context. Skipping status update.');
+    return;
+  }
+
+  const { sha, environment, projectName } = requiredContext;
+
+  const autoContext =
+    projectName && environment ? ` (${projectName} - ${environment})` : '';
+  const defaultStatusName = `${context.workflow} | ${context.job}${autoContext}`;
+  const contextForStatus = core.getInput('name') || defaultStatusName;
   core.debug(`status name: ${contextForStatus}`);
 
   if (stage === 'post') {
@@ -49,12 +80,15 @@ export async function setCommitStatus({
     per_page: 100,
   });
 
+  // find the job that matches the runId
   core.debug(`jobs: ${JSON.stringify(jobs, null, 2)}`);
+  core.debug(`context.runId: ${context.runId}`);
   const octokitJob = jobs.data.jobs.find(
-    (j: { name: string }) => j.name === context.job,
+    (j: (typeof jobs.data.jobs)[number]) => j.run_id === context.runId,
   );
+
   if (!octokitJob) {
-    throw new Error(`Job not found: ${context.job}`);
+    throw new Error(`Run not found: ${context.runId} (${context.job})`);
   }
 
   // Convert Octokit job to our Job type
@@ -65,9 +99,8 @@ export async function setCommitStatus({
       })) || [],
   };
 
-  // debug job
   const state = getStatusForJob({ stage, job });
-  core.debug(
+  core.info(
     `Setting commit status for SHA: ${sha}, state: ${state}, context: ${contextForStatus}`,
   );
 
